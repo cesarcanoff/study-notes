d665617f4109ec6f38f2b2972167626a35ad8e58
CesarCanoff <canoff.developer@gmail.com>
2022-03-01 14:53:50 -0400

    Project PHP [HTML5 Complete]

---
+++ Courses/Danki Code/Web Development Course/Module_09/Code/index.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Descrição do meu site!" />
+    <meta name="keywords" content="palavras, chave, por, virgula" />
+    <link rel="stylesheet" href="css/style.css" />
+    <title>Projeto Prático 01</title>
+  </head>
+
+  <body>
+    <header>
+      <div class="center">
+        <div class="logo">LOGO</div>
+        <nav class="desktop">
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Sobre</a></li>
+            <li><a href="#">Serviços</a></li>
+            <li><a href="#">Contatos</a></li>
+          </ul>
+        </nav>
+        <nav class="mobile">
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Sobre</a></li>
+            <li><a href="#">Serviços</a></li>
+            <li><a href="#">Contatos</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <section class="main-banner">
+      <div class="center">
+        <form action="">
+          <h2>Qual o seu melhor e-mail?</h2>
+          <input type="email" name="input_email" required />
+          <input type="submit" value="Cadastrar" />
+        </form>
+      </div>
+    </section>
+    <section class="author-description">
+      <div class="center">
+        <h2>César Canoff</h2>
+        <div class="w50">
+          <p>
+            Molestiae iusto maiores beatae. aboriosam molestiae iusto maiores
+            beatae. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Nam, repellat adipisci cumque non quia alias reiciendis officiis
+            odio minus magnam eligendi rem culpa laboriosam atque veniam,
+            officiis odio minus magnam eligendi rem culpa Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Nam repellat adipisci cumque non
+            quia alias reiciendis atque veniam
+          </p>
+        </div>
+      </div>
+
+      <div class="w50">
+        <img src="images/author_image.png" alt="Picture of Author" />
+      </div>
+    </section>
+    <section class="mainSkills">
+      <div class="center">
+        <h2 class="title">Especialidades</h2>
+        <div class="box-main-skills">
+          <h3>ICONE1</h3>
+          <h3>CSS3</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem
+            nesciunt quidem rem voluptates quo veniam conseq uuntur dicta
+          </p>
+        </div>
+        <div class="box-main-skills">
+          <h3>ICONE2</h3>
+          <h3>HTML5</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem
+            nesciunt quidem rem voluptates quo veniam conseq uuntur dicta
+          </p>
+        </div>
+        <div class="box-main-skills">
+          <h3>ICONE3</h3>
+          <h3>JAVASCRIPT</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem
+            nesciunt quidem rem voluptates quo veniam conseq uuntur dicta
+          </p>
+        </div>
+      </div>
+    </section>
+    <section class="extras">
+      <div class="center">
+        <div class="w50">
+          <h2 class="title">Depoimentos</h2>
+          <div class="depoimento-single">
+            <p class="depoimento-descricao">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem inventore tempore aliquid soluta fugit perspiciatis
+              sed. Fugiat delectus aut itaque, quisquam eaque rerum. Quae
+              asperiores tenetur maiores quo ab suscipit?
+            </p>
+            <p class="author-name">Lorem Ipsum</p>
+          </div>
+          <div class="depoimento-single">
+            <p class="depoimento-descricao">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem inventore tempore aliquid soluta fugit perspiciatis
+              sed. Fugiat delectus aut itaque, quisquam eaque rerum. Quae
+              asperiores tenetur maiores quo ab suscipit?
+            </p>
+            <p class="author-name">Lorem Ipsum</p>
+          </div>
+          <div class="depoimento-single">
+            <p class="depoimento-descricao">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem inventore tempore aliquid soluta fugit perspiciatis
+              sed. Fugiat delectus aut itaque, quisquam eaque rerum. Quae
+              asperiores tenetur maiores quo ab suscipit?
+            </p>
+            <p class="author-name">Lorem Ipsum</p>
+          </div>
+        </div>
+        <div class="w50">
+          <h2 class="title">Serviços</h2>
+          <div class="servicos">
+            <ul>
+              <li>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Mollitia enim dolor repellat deleniti voluptatem, distinctio
+                voluptates reprehenderit corporis recusandae debitis. Ullam
+                molestias amet minus. Velit quis excepturi ratione eos unde.
+              </li>
+              <li>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Mollitia enim dolor repellat deleniti voluptatem, distinctio
+                voluptates reprehenderit corporis recusandae debitis. Ullam
+                molestias amet minus. Velit quis excepturi ratione eos unde.
+              </li>
+              <li>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Mollitia enim dolor repellat deleniti voluptatem, distinctio
+                voluptates reprehenderit corporis recusandae debitis. Ullam
+                molestias amet minus. Velit quis excepturi ratione eos unde.
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+    <footer>
+      <div class="center">
+        <p>Todos os Direitos Reservados.</p>
+      </div>
+    </footer>
+  </body>
+</html>
