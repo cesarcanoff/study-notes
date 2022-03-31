847318e789226d2dc11055c91c88a0ffa3a1b08e
CesarCanoff <canoff.developer@gmail.com>
2022-03-31 16:50:17 -0400

    Adding Part 04 of Project Creating your own Tic Tac Toe with HTML and Javascript

--- Courses/Digital Innovation One/Project: Creating your own Tic Tac Toe with HTML and Javascript/Code/js/scripts.js
+++ Courses/Digital Innovation One/Project: Creating your own Tic Tac Toe with HTML and Javascript/Code/js/scripts.js
@@ -1,5 +1,15 @@
 var player;
+var selectPlayer = document.getElementById('selectPlayer');
+
+changePlayer('X');
 
 function chooseSquare(elementId) {
-    
+    let square = document.getElementById(elementId);
+    square.innerHTML = player;
+    square.style.color = 'green';
+}
+
+function changePlayer(value) {
+  player = value;
+  selectPlayer.innerHTML = player;
 }
