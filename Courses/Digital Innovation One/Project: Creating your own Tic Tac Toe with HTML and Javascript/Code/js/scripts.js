8cff8ac740fba15cc0c25db43c971125c2b428d5
CesarCanoff <canoff.developer@gmail.com>
2022-03-31 21:07:31 -0400

    Fix Code

--- Courses/Digital Innovation One/Project: Creating your own Tic Tac Toe with HTML and Javascript/Code/js/scripts.js
+++ Courses/Digital Innovation One/Project: Creating your own Tic Tac Toe with HTML and Javascript/Code/js/scripts.js
@@ -1,104 +1,104 @@
 var player, winner = null;
-var selectPlayer = document.getElementById('selectPlayer')
-var selectWinner = document.getElementById('selectWinner')
+var selectPlayer = document.getElementById('selectPlayer');
+var selectWinner = document.getElementById('selectWinner');
 
-changePlayer('X')
+changePlayer('X');
 
 function chooseSquare(elementId) {
 
   if (winner !== null) {
     return;
   }
-  let square = document.getElementById(elementId)
+  let square = document.getElementById(elementId);
   if (square.innerHTML === '-') {
-    square.innerHTML = player
-    square.style.color = 'green'
+    square.innerHTML = player;
+    square.style.color = 'green';
     if (player === 'X') {
-      player = 'O'
+      player = 'O';
     } else {
-      player = 'X'
+      player = 'X';
     }
   } else {
-    return
+    return;
   }
-  changePlayer(player)
-  winnerCheck()
+  changePlayer(player);
+  winnerCheck();
 }
 
 function changePlayer(value) {
-  player = value
-  selectPlayer.innerHTML = player
+  player = value;
+  selectPlayer.innerHTML = player;
 }
 
 function winnerCheck() {
-  var square_1 = document.getElementById(1)
-  var square_2 = document.getElementById(2)
-  var square_3 = document.getElementById(3)
-  var square_4 = document.getElementById(4)
-  var square_5 = document.getElementById(5)
-  var square_6 = document.getElementById(6)
-  var square_7 = document.getElementById(7)
-  var square_8 = document.getElementById(8)
-  var square_9 = document.getElementById(9)
+  var square_1 = document.getElementById(1);
+  var square_2 = document.getElementById(2);
+  var square_3 = document.getElementById(3);
+  var square_4 = document.getElementById(4);
+  var square_5 = document.getElementById(5);
+  var square_6 = document.getElementById(6);
+  var square_7 = document.getElementById(7);
+  var square_8 = document.getElementById(8);
+  var square_9 = document.getElementById(9);
 
   if (checkSequence(square_1, square_2, square_3)) {
-    changeSquareColor(square_1, square_2, square_3)
-    changeWinner(square_1)
+    changeSquareColor(square_1, square_2, square_3);
+    changeWinner(square_1);
     return;
   } 
   
   if (checkSequence(square_4, square_5, square_6)) {
-    changeSquareColor(square_4, square_5, square_6)
-    changeWinner(square_4)
+    changeSquareColor(square_4, square_5, square_6);
+    changeWinner(square_4);
     return;
   }
   
   if (checkSequence(square_7, square_8, square_9)) {
-    changeSquareColor(square_7, square_8, square_9)
-    changeWinner(square_7)
+    changeSquareColor(square_7, square_8, square_9);
+    changeWinner(square_7);
     return;
   }
   
   if (checkSequence(square_3, square_5, square_7)) {
-    changeSquareColor(square_3, square_5, square_7)
+    changeSquareColor(square_3, square_5, square_7);
     changeWinner(square_3);
     return;
   }
   
   if (checkSequence(square_1, square_5, square_9)) {
-    changeSquareColor(square_1, square_5, square_9)
+    changeSquareColor(square_1, square_5, square_9);
     changeWinner(square_1);
     return;
   }
   
   if (checkSequence(square_1, square_4, square_7)) {
-    changeSquareColor(square_1, square_4, square_7)
+    changeSquareColor(square_1, square_4, square_7);
     changeWinner(square_1);
     return;
   }
   
   if (checkSequence(square_2, square_5, square_8)) {
-    changeSquareColor(square_2, square_5, square_8)
+    changeSquareColor(square_2, square_5, square_8);
     changeWinner(square_2);
     return;
   }
   
   if (checkSequence(square_3, square_6, square_9)) {
-    changeSquareColor(square_3, square_6, square_9)
+    changeSquareColor(square_3, square_6, square_9);
     changeWinner(square_3);
     return;
   }
 }
 
 function changeSquareColor(firstSquare, secondSquare, thirdSquare) {
-  firstSquare.style.backgroundColor = '#0f0'
-  secondSquare.style.backgroundColor = '#0f0'
-  thirdSquare.style.backgroundColor = '#0f0'
+  firstSquare.style.backgroundColor = '#0f0';
+  secondSquare.style.backgroundColor = '#0f0';
+  thirdSquare.style.backgroundColor = '#0f0';
 }
 
 function changeWinner(square) {
-  winner = square.innerHTML
-  selectWinner.innerHTML = winner
+  winner = square.innerHTML;
+  selectWinner.innerHTML = winner;
 }
 
 function checkSequence(firstSquare, secondSquare, thirdSquare) {
@@ -107,7 +107,7 @@ function checkSequence(firstSquare, secondSquare, thirdSquare) {
     firstSquare.innerHTML === secondSquare.innerHTML &&
     secondSquare.innerHTML === thirdSquare.innerHTML
   ) {
-    return true
+    return true;
   }
 }
 
