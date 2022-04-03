e7876ad9fad8a2e307e6c9dd3341ff010380d873
CesarCanoff <canoff.developer@gmail.com>
2022-04-03 01:21:24 -0400

    Creating your Space Shooter Game 7/8

--- Courses/Digital Innovation One/Creating your Space Shooter Game/js/scripts.js
+++ Courses/Digital Innovation One/Creating your Space Shooter Game/js/scripts.js
@@ -61,7 +61,15 @@ function createLaserElement () {
 function moveLaser (laser) {
   let laserInterval = setInterval(() => {
     let xPosition = parseInt(laser.style.left);
-    
+    let aliens = document.querySelectorAll('.alien');
+
+    alien.array.forEach((alien) => { // COMPARANDO SE CADA ALIEN PRESENTE FOOI ATINGIDO
+      if (checkLaserCollision(laser, alien)) {
+        alien.src =  '../images/explosion.png';
+        alien.classList.remove('alien');
+        alien.classList.add('dead-alien');
+      }
+    });
 
     if (xPosition === 340) {
       laser.remove();
@@ -84,4 +92,41 @@ function createAliens () {
   moveAlien(newAlien);
 }
 
+// FUNÇÃO MOVE ALIEN
+function moveAlien(alien) {
+  let moveAlienInterval = setInterval(() => {
+    let xPosition = parseInt(window.getComputedStyle(alien).getPropertyValue('left'));
+    if (xPosition <= 50) {
+      if (Array.from(alien.classList).includes('dead-alien')) {
+        alien.remove();
+      } else {
+        // gameOver();
+      }
+    } else {
+      alien.style.left = `${xPosition - 4}px`;
+    }
+  }, 30)
+}
+
+// FUNÇÃO COLISÃO
+function checkLaserCollision (laser, alien) {
+  let laserTop = parseInt(laser.style.top);
+  let laserLeft = parseInt(laser.style.left);
+  let laserBottom = laserTop - 20;
+  let alienTop = parseInt(alien.style.top);
+  let alienLeft = parseInt(alien.style.left);
+  let alienBottom = alienTop - 30;
+
+  if (laserLeft != 340 && laserLeft + 40 >= alienLeft) {
+    if (laserTop <= alienTop && laserTop >= alienBottom) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
+
 window.addEventListener('keydown', flyShip);
+createAliens()
