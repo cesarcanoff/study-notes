a9c3392ca22475a03ff62ba6799163a595ef4f82
CesarCanoff <canoff.developer@gmail.com>
2022-04-03 00:33:17 -0400

    Creating your Space Shooter Game 3/8

--- Courses/Digital Innovation One/Creating your Space Shooter Game/js/scripts.js
+++ Courses/Digital Innovation One/Creating your Space Shooter Game/js/scripts.js
@@ -26,3 +26,17 @@ function moveUp() {
     yourShip.style.top = `${position}px`;
   }
 }
+
+// FUNÇÃO DESCER
+function moveDown () {
+  let topPosition = getComputedStyle(yourShip).getPropertyValue('top');
+  if (topPosition === '520px') {
+    return;
+  } else {
+    let position = parseInt(topPosition);
+    position += 50;
+    yourShip.style.top = `${position}px`;
+  }
+}
+
+window.addEventListener('keydown', flyShip);
