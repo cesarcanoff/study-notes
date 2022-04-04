625d058af689d68802d46a1060f0add524913155
CesarCanoff <canoff.developer@gmail.com>
2022-04-03 21:02:04 -0400

    Building Your First Ship Game Step 04

--- Courses/Digital Innovation One/Building Your First Ship Game/js/scripts.js
+++ Courses/Digital Innovation One/Building Your First Ship Game/js/scripts.js
@@ -1,7 +1,7 @@
 function start() {
 	$("#inicio").hide();
-	$("#fundoGame").append("<div id='jogador'></div>");
-	$("#fundoGame").append("<div id='inimigo1'></div>");
+	$("#fundoGame").append("<div class='anima1' id='jogador'></div>");
+	$("#fundoGame").append("<div class='anima2' id='inimigo1'></div>");
 	$("#fundoGame").append("<div id='inimigo2'></div>");
-	$("#fundoGame").append("<div id='amigo'></div>");
+	$("#fundoGame").append("<div class='anima3' id='amigo'></div>");
 }
