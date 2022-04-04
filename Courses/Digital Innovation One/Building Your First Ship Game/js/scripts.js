1d9399225f0725c544710e575d3455fd321b21da
CesarCanoff <canoff.developer@gmail.com>
2022-04-03 21:11:22 -0400

    Building Your First Ship Game Step 09

--- Courses/Digital Innovation One/Building Your First Ship Game/js/scripts.js
+++ Courses/Digital Innovation One/Building Your First Ship Game/js/scripts.js
@@ -99,3 +99,34 @@ function moveamigo() {
     $('#amigo').css('left', 0)
   }
 }
+
+var podeAtirar = true
+disparo()
+
+function disparo() {
+  if (podeAtirar == true) {
+    podeAtirar = false
+
+    topo = parseInt($('#jogador').css('top'))
+    posicaoX = parseInt($('#jogador').css('left'))
+    tiroX = posicaoX + 190
+    topoTiro = topo + 37
+    $('#fundoGame').append("<div id='disparo'></div")
+    $('#disparo').css('top', topoTiro)
+    $('#disparo').css('left', tiroX)
+
+    var tempoDisparo = window.setInterval(executaDisparo, 30)
+  }
+
+  function executaDisparo() {
+    posicaoX = parseInt($('#disparo').css('left'))
+    $('#disparo').css('left', posicaoX + 15)
+
+    if (posicaoX > 900) {
+      window.clearInterval(tempoDisparo)
+      tempoDisparo = null
+      $('#disparo').remove()
+      podeAtirar = true
+    }
+  }
+}
