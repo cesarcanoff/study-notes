c6b852e78196d32bacecc0683ec7451fc71ed421
CesarCanoff <canoff.developer@gmail.com>
2022-04-04 17:12:49 -0400

    Dinosaur Game 6/8

--- Courses/Digital Innovation One/Dinosaur Game/js/script.js
+++ Courses/Digital Innovation One/Dinosaur Game/js/script.js
@@ -11,36 +11,47 @@ function handleKeyUp(event) {
 }
 
 function jump() {
-  let position = 0
+  let position = 0;
   isJump = true;
   let upInterval = setInterval(() => {
     if (position >= 150) {
       clearInterval(upInterval);
       let downInterval = setInterval(() => {
-        if (position <= 20 ) {
+        if (position <= 20) {
           clearInterval(downInterval);
           isJump = false;
         } else {
           position -= 20;
-          dino.style.bottom = `${position}px`
+          dino.style.bottom = `${position}px`;
         }
       }, 20);
     } else {
-      position += 20
-      dino.style.bottom = `${position}px`
+      position += 20;
+      dino.style.bottom = `${position}px`;
     }
   }, 20)
 }
 
-function createCactus () {
+function createCactus() {
   const cactus = document.createElement('div');
   let cactusPosition = 1000;
+  let randomTime = Math.random() * 5000;
 
   cactus.classList.add('cactus');
   cactus.style.left = `${1000}px`;
   background.appendChild(cactus);
+
+  let leftInterval = setInterval(() => {
+    if (cactusPosition < -60) {
+      clearInterval(leftInterval);
+      background.removeChild(cactus);
+    } else {
+      cactusPosition -= 10;
+      cactus.style.left = `${cactusPosition}px`;
+    }
+  }, 20);
+  setTimeout(createCactus, randomTime);
 }
 
-
 createCactus();
-document.addEventListener('keyup', handleKeyUp)
+document.addEventListener('keyup', handleKeyUp);
