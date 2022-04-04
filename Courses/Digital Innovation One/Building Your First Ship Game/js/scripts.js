ead7ef57d9a1c34024234bfad5a6e74e5fc50b2a
CesarCanoff <canoff.developer@gmail.com>
2022-04-03 21:21:09 -0400

    Building Your First Ship Game Step 17

--- Courses/Digital Innovation One/Building Your First Ship Game/js/scripts.js
+++ Courses/Digital Innovation One/Building Your First Ship Game/js/scripts.js
@@ -337,3 +337,53 @@ function energia() {
 }
 
 velocidade = velocidade + 0.3
+
+var somDisparo = document.getElementById('somDisparo')
+var somExplosao = document.getElementById('somExplosao')
+var musica = document.getElementById('musica')
+var somGameover = document.getElementById('somGameover')
+var somPerdido = document.getElementById('somPerdido')
+var somResgate = document.getElementById('somResgate')
+
+musica.addEventListener(
+  'ended',
+  function () {
+    musica.currentTime = 0
+    musica.play()
+  },
+  false
+)
+musica.play()
+
+somDisparo.play()
+
+somResgate.play()
+
+somExplosao.play()
+
+somPerdido.play()
+
+function gameOver() {
+  fimdejogo = true
+  musica.pause()
+  somGameover.play()
+
+  window.clearInterval(jogo.timer)
+  jogo.timer = null
+
+  $('#jogador').remove()
+  $('#inimigo1').remove()
+  $('#inimigo2').remove()
+  $('#amigo').remove()
+
+  $('#fundoGame').append("<div id='fim'></div>")
+
+  $('#fim').html(
+    '<h1> Game Over </h1><p>Sua pontuação foi: ' +
+      pontos +
+      '</p>' +
+      "<div id='reinicia' onClick=reiniciaJogo()><h3>Jogar Novamente</h3></div>"
+  )
+}
+
+gameOver()
