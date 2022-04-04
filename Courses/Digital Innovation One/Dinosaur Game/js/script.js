28265a2d7744cc4f8c76f519875d36597976e5af
CesarCanoff <canoff.developer@gmail.com>
2022-04-04 17:01:53 -0400

    Dinosaur Game 5/8

--- Courses/Digital Innovation One/Dinosaur Game/js/script.js
+++ Courses/Digital Innovation One/Dinosaur Game/js/script.js
@@ -1,19 +1,25 @@
-const dino = document.querySelector('.dino')
+const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
+let isJump = false;
 
 function handleKeyUp(event) {
   if (event.keyCode === 32) {
-    jump()
+    if (!isJump) {
+      jump();
+    }
   }
 }
 
 function jump() {
   let position = 0
+  isJump = true;
   let upInterval = setInterval(() => {
     if (position >= 150) {
       clearInterval(upInterval);
       let downInterval = setInterval(() => {
         if (position <= 20 ) {
           clearInterval(downInterval);
+          isJump = false;
         } else {
           position -= 20;
           dino.style.bottom = `${position}px`
@@ -26,4 +32,15 @@ function jump() {
   }, 20)
 }
 
+function createCactus () {
+  const cactus = document.createElement('div');
+  let cactusPosition = 1000;
+
+  cactus.classList.add('cactus');
+  cactus.style.left = `${1000}px`;
+  background.appendChild(cactus);
+}
+
+
+createCactus();
 document.addEventListener('keyup', handleKeyUp)
