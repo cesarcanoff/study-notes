9b72eee6488c91bd4482f35c85efe00784d1cda8
CesarCanoff <canoff.developer@gmail.com>
2022-04-12 19:50:24 -0400

    Rename files from .html to .ejs

--- Content/Danki Code/NodeJS Course/Module 08 - Static News Site Project/Code/index.js
+++ Content/Danki Code/NodeJS Course/Module 08 - Static News Site Project/Code/index.js
@@ -12,7 +12,7 @@ app.use(
 );
 
 app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+app.set('view engine', 'ejs');
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, '/pages'));
 
