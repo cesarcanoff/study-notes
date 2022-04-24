9a1498371815088db6dde0dc61434029badcf68d
CesarCanoff <canoff.developer@gmail.com>
2022-04-24 16:05:03 -0400

    Adding Parking System Using TypeScript 3/5

--- Content/Digital Innovation One/typescript-project/script.ts
+++ Content/Digital Innovation One/typescript-project/script.ts
@@ -1,9 +1,41 @@
-(() => {
-  const $ = (query: string): HTMLInputElement | null => document.querySelector(query);
-  $('#cadastrar')?.addEventListener('click', () => {
-    const nome = $('#nome')?.value;
-    const placa = $('#placa')?.value;
+interface Veiculo {
+  nome: string,
+  placa: string,
+  entrada: Date,
+}
 
-    if (!nome || !placa) alert('Os campos NOME e PLACA são obrigatórios!');
+;(() => {
+  const $ = (query: string): HTMLInputElement | null =>
+    document.querySelector(query)
+
+  function patio() {
+    function ler() {}
+    function adicionar(veiculo: Veiculo) {
+      const row = document.createElement("tr");
+      row.innerHTML = `
+        <td>${veiculo.nome}</td>
+        <td>${veiculo.placa}</td>
+        <td>${veiculo.entrada}</td>
+        <td>
+        <button class="delete" data-placa="${veiculo.placa}">X</button>
+        </td>
+      `;
+
+      $('#patio')?.appendChild(row)
+    }
+    function remover() {}
+    function salvar() {}
+    function renderizar() {}
+
+    return { ler, adicionar, remover, salvar, renderizar };
+  }
+
+  $('#cadastrar')?.addEventListener('click', () => {
+    const nome = $('#nome')?.value
+    const placa = $('#placa')?.value
+
+    if (!nome || !placa) alert('Os campos NOME e PLACA são obrigatórios!')
+
+    patio().adicionar({nome, placa, entrada: new Date()})
   })
-})();
+})()
