4c8e0596304ffa2fda26edc7ecfc74e4ca5b0644
CesarCanoff <canoff.developer@gmail.com>
2022-04-23 20:29:27 -0400

    JavaScript - Working with Modules in JavaScript Course

---
+++ Content/Digital Innovation One/JavaScript - Working with Modules in JavaScript/Notes/main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('César', 18));
+console.log(mostraIdade('César', 'Naviraí'));
+console.log(mostraHobby('César', 'Robótica'));
